87ae3e8cb879974d4b99b239f2d70ffc39235751
Maciej Majchrowski <maciek21197@gmail.com>
2018-11-14 23:45:22 +0100

    begining v2.

---
+++ DSAAlgorythm/Signature.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSAAlgorythm
+{
+    public class Signature
+    {
+        public BigInt R { get; set; }
+        public BigInt S { get; set; }
+        public Signature(BigInt r, BigInt s)
+        {
+            R = r;
+            S = s;
+        }
+    }
+}
