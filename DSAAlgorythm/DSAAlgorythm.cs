82f98ec08505172dcf6762192aa166b09b9c7cfd
Maciej Majchrowski <maciek21197@gmail.com>
2018-11-16 02:23:31 +0100

    Created model, before adding algorythm

--- DSAAlgorythm/DSAAlgorythm.cs
+++ DSAAlgorythm/DSAAlgorythm.cs
@@ -1,28 +1,41 @@
-﻿using System;
+﻿using DSAAlgorythm.Data;
+using System;
+using System.Security.Cryptography;
+using static DSAAlgorythm.Hasher;
 
 namespace DSAAlgorythm
 {
 
     public class DSAAlgorythm
     {
-        public BigInt P { get; set; }
-        public BigInt G { get; set; }
-        public BigInt Q { get; set; }
-        public BigInt PrivateKey { get; set; }
-        public BigInt PublicKey { get; set; }
-        public BigInt K { get; set; }
+        public BigInteger P { get; set; }
+        public BigInteger G { get; set; }
+        public BigInteger Q { get; set; }
+        public BigInteger R{ get; set; }
+        public BigInteger S { get; set; }
+        public BigInteger K { get; set; }
+        public BigInteger PrivateKey { get; set; }
+        public BigInteger PublicKey { get; set; }
+        public BigInteger Hash { get; set; }
+        private IDataProvider Data { get; }
+        public byte[] BytesMessage { get; set; }
+        private Hasher HasherImplementation { get; }
 
-        public BigInt Hash { get; set; }
 
+        public DSAAlgorythm(IDataProvider dataProvider, HashImplementation hashImplementation)
+        {
+            BytesMessage = dataProvider.GetData();
+            HasherImplementation = new Hasher(hashImplementation);
+            Hash = new BigInteger(HasherImplementation.GetHashedMessage(BytesMessage));
+        }
 
         public Signature Signing()
         {
-            BigInt r, s;
-            r = (Math.Pow(G, K) % P) % Q;
+            //R = (Math.Pow(G, K) % P) % Q;
             // TODO power operation
-            s = [(Math.Pow(K, -1.0) * (Hash + PrivateKey * r))] % Q;
+            S = [(Math.Pow(K, -1.0) * (Hash + PrivateKey * r))] % Q;
             // TODO revert BigInt Operation
-            return new Signature(r, s);
+            return new null;
         }
 
         public Verifying(Signature signature, )
---
+++ DSAAlgorythm/Helpers/TextUtility.cs
@@ -0,0 +1,55 @@
+﻿using System.ComponentModel;
+
+namespace DSAAlgorythm.Helpers
+{
+    public static class TextUtility
+    {
+        public enum Encoding
+        {
+            [Description("ASCII")]
+            Ascii,
+
+            [Description("UTF-8")]
+            Utf8,
+
+            [Description("Unicode")]
+            Unicode
+        }
+
+        public static byte[] ToByteArray(this string text, Encoding encoding)
+        {
+            switch (encoding)
+            {
+                case Encoding.Ascii:
+                    return System.Text.Encoding.ASCII.GetBytes(text);
+
+                case Encoding.Unicode:
+                    return System.Text.Encoding.Unicode.GetBytes(text);
+
+                case Encoding.Utf8:
+                    return System.Text.Encoding.UTF8.GetBytes(text);
+
+                default:
+                    return null;
+            }
+        }
+
+        public static string ToText(this byte[] data, Encoding encoding)
+        {
+            switch (encoding)
+            {
+                case Encoding.Ascii:
+                    return System.Text.Encoding.ASCII.GetString(data);
+
+                case Encoding.Unicode:
+                    return System.Text.Encoding.Unicode.GetString(data);
+
+                case Encoding.Utf8:
+                    return System.Text.Encoding.UTF8.GetString(data);
+
+                default:
+                    return null;
+            }
+        }
+    }
+}
